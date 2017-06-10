15f8359cc840c18fb27c1c351c523d4c5a59efc9
范志康 <goodman.capu@gmail.com>
2017-06-10 15:44:12 +0800

    3.6.3

--- CAPUBBS/AppDelegate.m
+++ CAPUBBS/AppDelegate.m
@@ -365,6 +365,7 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
+    [[AFNetworkReachabilityManager sharedManager] stopMonitoring];
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
@@ -386,6 +387,7 @@
         [self _loginAsync:YES];
     }
     [DEFAULTS setObject:[NSNumber numberWithBool:YES] forKey:@"wakeLogin"];
+    [[AFNetworkReachabilityManager sharedManager] startMonitoring];
 
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
--- Common Files/CommonDefinitions.h
+++ Common Files/CommonDefinitions.h
@@ -14,7 +14,7 @@
 #define APP_GROUP_IDENTIFIER @"group.net.chexie.capubbs"
 
 #define REPORT_EMAIL @[@"beidachexie@163.com"]
-#define FEEDBACK_EMAIL @[@"goodman.capu@qq.com", @"beidachexie@163.com"]
+#define FEEDBACK_EMAIL @[@"goodman.capu@gmail.com", @"beidachexie@163.com"]
 #define COPYRIGHT @"Copyright®  2001 - 2017\nPowered by：CAPU ver 3.0"
 #define EULA @"本论坛作为北京大学自行车协会内部以及自行车爱好者之间交流平台，不欢迎任何商业广告和无关话题。发言者对自己发表的任何言论、信息负责。"
 
